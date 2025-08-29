af20358e92568b79511102e76d48fbddd5d9712a
Syndromehsh <2872935391@qq.com>
2025-08-29 08:03:34 +0800

    Update and rename Main Script.txt to Main Script.lua

---
+++ xiaoyue/Main Script.lua
@@ -0,0 +1,10 @@
+
+  if game.PlaceId == 126509999114328 then --- 99夜
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Syndromehsh/bypass-Script/refs/heads/main/xiaoyue/99%20Night.txt"))()
+elseif game.PlaceId == 2753915549 then --- Blox Fruit
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Syndromehsh/bypass-Script/refs/heads/main/xiaoyue/Blox%20Fruit.txt"))()
+elseif game.PlaceId == 18687417158 then --- 被遗弃
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Syndromehsh/bypass-Script/refs/heads/main/xiaoyue/Forsaken.txt"))()
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SyndromeXph/BETA/refs/heads/main/TY"))()
+end
